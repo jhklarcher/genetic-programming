80cff772fd188a82735c4deb42eac3af6bc59658
jhklarcher <jhklarcher@gmail.com>
2023-12-27 00:26:48 -0300

    Update repo organization.

--- genetic_programming/genetic_programming.py
+++ genetic_programming/genetic_programming.py
@@ -1,10 +1,6 @@
-import numpy as np
-import operator
-import math
-import random
 import copy
-import matplotlib.pyplot as plt
-from sympy import symbols, sin, cos, simplify
+import random
+import numpy as np
 
 random.seed(42)
 
@@ -138,7 +134,6 @@ def value_mutation(tree, functions, terminals):
 cache = {}
 cache_size = 1000
 
-
 def fitness(tree, X, y, depth_coefficient):
     # Check if the tree has already been evaluated
     if tree.expression() in cache:
@@ -217,67 +212,3 @@ def gp_minimize(X, y, functions, terminals, max_depth=None, population_size=100,
     # Return the best individual
     return sorted(population, key=lambda tree: fitness(tree, X, y, depth_coefficient))[-1]
 
-
-def protected_division(x, y):
-    return x / y if y != 0 else 1
-
-
-functions = {
-    operator.add: 2,
-    operator.sub: 2,
-    operator.mul: 2,
-    # protected_division: 2,
-    np.sin: 1,
-    np.cos: 1
-}
-
-# Define your terminals - these could be constants and variables like 'x'.
-terminals = ['x_0', 10]
-
-max_depth = None  # The maximum depth of the tree
-n_pop = 500  # The number of individuals in the population
-n_generations = 100  # The number of generations
-crossover_probability = 0.9  # The probability of crossover
-mutation_probability = 0.1  # The probability of mutation
-depth_coefficient = 0.1  # The coefficient for the depth penalty
-tournament_size = 5  # The tournament size for tournament selection
-
-# Generate data
-x_0 = np.linspace(-10, 10, 300).reshape(-1, 1)
-y = x_0**2 + 10*np.cos(x_0*10) + 10
-
-X = x_0
-y = y.reshape(-1, 1)
-plt.plot(X, y, label='True')
-
-
-res = gp_minimize(X, y, functions, terminals, max_depth, population_size=n_pop, n_generations=n_generations, crossover_probability=crossover_probability,
-                  mutation_probability=mutation_probability, depth_coefficient=depth_coefficient, tournament_size=tournament_size)
-
-y_pred = [res.evaluate({f'x_{i}': x_i for i, x_i in enumerate(row)})
-          for row in X]
-
-plt.plot(X, y, label='True')
-plt.plot(X, y_pred, label='Predicted')
-plt.legend()
-plt.show()
-
-print(res.expression())
-print(res.depth())
-
-sympy_functions = {
-    operator.add: lambda x, y: x + y,
-    operator.sub: lambda x, y: x - y,
-    operator.mul: lambda x, y: x * y,
-    protected_division: lambda x, y: x / y,
-    np.sin: lambda x: sin(x),
-    np.cos: lambda x: cos(x)
-}
-
-x_0 = symbols('x')
-
-x = res.sympy_expression({f'x_0': x_0}, sympy_functions)
-
-x
-
-simplify(x)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genetic_programming',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+)
